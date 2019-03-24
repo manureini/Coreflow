84e610b8728a8be0bd1d0f735f34ea290d4b2b24
lordmampf <lordmampf@users.noreply.github.com>
2019-03-24 23:53:13 +0100

    Update Program.cs

--- Coreflow.Web/Program.cs
+++ Coreflow.Web/Program.cs
@@ -16,6 +16,7 @@ namespace Coreflow.Web
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>();
+                .UseStartup<Startup>()
+                .UseUrls("http://0.0.0.0:5700");
     }
 }
