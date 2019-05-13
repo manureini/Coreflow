e5fe2c9ea71064d11c9e041231e62cfa009e030a
lordmampf <lordmampf@users.noreply.github.com>
2019-05-13 18:07:07 +0200

    more missing files

---
+++ src/Coreflow/CodeCreators/DebugCreator/PrintVariablesCreator.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Linq;
+using Coreflow.Interfaces;
+using Coreflow.Objects;
+using Microsoft.CodeAnalysis;
+using Microsoft.Extensions.Logging;
+
+namespace Coreflow.CodeCreators
+{
+    public class PrintVariablesCreator : ICodeCreator, IUiDesignable
+    {
+        public Guid Identifier { get; set; } = Guid.NewGuid();
+
+        public string FactoryIdentifier { get; set; }
+
+        public string Name => "Print Variables";
+
+        public string Icon => "fa-bug";
+
+        public string Category => "Debug";
+
+        public void ToCode(FlowBuilderContext pBuilderContext, FlowCodeWriter pCodeWriter, ICodeCreatorContainerCreator pContainer = null)
+        {
+            pBuilderContext.UpdateCurrentSymbols();
+
+            pCodeWriter.WriteIdentifierTagTop(this);
+
+            // pBuilderContext.CurrentSymbols
+
+            var symbols = pBuilderContext.CurrentSymbols
+                .Where(s => s.Kind == SymbolKind.Property || s.Kind == SymbolKind.Local || s.Kind == SymbolKind.Field);
+            
+            string message = "Current Variables: \" + global::System.Environment.NewLine + \"";
+
+            foreach (var symbol in symbols)
+            {
+                message += $"    {symbol.Name}: \" + {symbol.Name} + global::System.Environment.NewLine + \"";
+
+            }
+
+            pCodeWriter.AppendLoggingCode(LogLevel.Debug, message);
+        }
+    }
+}
