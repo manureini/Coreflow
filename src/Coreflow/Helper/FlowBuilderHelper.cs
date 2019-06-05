7e58d2c5f8d895101f2dee80d092d4e3e0db2f43
lordmampf <lordmampf@users.noreply.github.com>
2019-06-05 18:54:11 +0200

    target is not working on linux

--- src/Coreflow/Helper/FlowBuilderHelper.cs
+++ src/Coreflow/Helper/FlowBuilderHelper.cs
@@ -52,7 +52,7 @@ namespace Coreflow
 
             string flowid = pFlowDefinition.Identifier.ToString().ToVariableName();
 
-            cw.AppendLineTop("[assembly: global::System.Runtime.Versioning.TargetFrameworkAttribute(\".NETCoreApp, Version = v3.0\", FrameworkDisplayName = \"\")]");
+         //   cw.AppendLineTop("[assembly: global::System.Runtime.Versioning.TargetFrameworkAttribute(\".NETCoreApp, Version = v3.0\", FrameworkDisplayName = \"\")]");
 
             cw.AppendLineTop("namespace " + FLOW_NAMESPACE_PREFIX + flowid + " {");
 
