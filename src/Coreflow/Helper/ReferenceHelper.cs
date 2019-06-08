48a7bdd7549cf1d332219d1ace7eff20a61ac76f
lordmampf <lordmampf@users.noreply.github.com>
2019-06-08 17:44:43 +0200

    Update ReferenceHelper.cs

--- src/Coreflow/Helper/ReferenceHelper.cs
+++ src/Coreflow/Helper/ReferenceHelper.cs
@@ -40,9 +40,8 @@ namespace Coreflow.Helper
             if (pRuntimeAssembly.Contains(".Private."))
                 return null;
 
-
-           //if (pRuntimeAssembly.Contains("netstandard"))
-           //     return null;
+            if (pRuntimeAssembly.Contains("netstandard"))
+                return null;
 
             string dllFileName = Path.GetFileName(pRuntimeAssembly);
 
